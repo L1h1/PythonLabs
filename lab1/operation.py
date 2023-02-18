eb29bd0ce8436ef4272878474711a38a05e7f6b1
L1h1 <i.aleksandrionok@gmail.com>
2023-02-18 17:09:13 +0300

    added operation.py

---
+++ lab1/operation.py
@@ -0,0 +1,9 @@
+def calc(num1: int|float,num2: int|float,op: str):
+    if op=="add":
+        return num1+num2
+    elif op=="sub":
+        return num1-num2
+    elif op=="mult":
+        return num1*num2
+    elif op=="div":
+        return num1/num2
