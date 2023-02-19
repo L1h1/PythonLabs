b524c0aee8ce76518ac2798f21b05eb9cdccdbaa
L1h1 <i.aleksandrionok@gmail.com>
2023-02-19 20:10:47 +0300

    non-declarative sentences

--- lab2/task1/utilities.py
+++ lab2/task1/utilities.py
@@ -2,15 +2,23 @@ import re
 
 
 def sentences_amount(text:str):
-    return len([element for element in re.split(r"[.!?]",text) if len(element.split())])
+    return len([element for element in re.split(r"[.!?]+",text) if len(element.split())])
 
 
 def non_declarative_sentences_amount(text:str):
-    pass
+    text=text.replace('.','')
+    potential_ans=len([element for element in re.split(r"[!?]+",text) if len(element.split())])
+    if potential_ans==1:
+        if not '!' in text and not '?' in text:
+            return 0
+        else:
+            return potential_ans
+    return potential_ans if text[len(text)-1]=='?' or text[len(text)-1]=='!' else potential_ans-1
+
 
 
 def average_sentence_len(text:str):
-    sentences_lengths = [len(element) for element in re.split(r"[.!?]",text) if len(element.split())]
+    sentences_lengths = [len(element) for element in re.split(r"[.!?]+",text) if len(element.split())]
     return 0 if not len(sentences_lengths) else sum(sentences_lengths)/len(sentences_lengths)
 
 
