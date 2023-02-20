c44204d77d8e9fda34756e7beb9e9f5fb46147e6
L1h1 <i.aleksandrionok@gmail.com>
2023-02-20 14:56:34 +0300

    basic storage skeleton

---
+++ lab2/task2/storage.py
@@ -0,0 +1,36 @@
+class Storage:
+    
+    data = {}
+    cur_user = None
+    
+    
+    def add(self, *key_args):
+        pass
+    
+    
+    def remove(self, key):
+        pass
+
+
+    def find(self, key):
+        pass
+
+
+    def lst(self):
+        pass
+
+
+    def grep(self, filter):
+        pass
+
+
+    def save(self):
+        pass
+
+
+    def load(self):
+        pass
+
+
+    def switch(self, username:str):
+        pass
