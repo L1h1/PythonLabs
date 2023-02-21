e238aa1998ecd1b0ba7a905e2f4ed236c1ce8d6a
L1h1 <i.aleksandrionok@gmail.com>
2023-02-21 15:48:39 +0300

    ngrams

--- lab2/task1/utilities.py
+++ lab2/task1/utilities.py
@@ -8,11 +8,13 @@ def sentences_amount(text:str):
 def non_declarative_sentences_amount(text:str):
     text=text.replace('.','')
     potential_ans=len([element for element in re.split(r"[!?]+",text) if len(element.split())])
+
     if potential_ans==1:
         if not '!' in text and not '?' in text:
             return 0
         else:
             return potential_ans
+            
     return potential_ans if text[len(text)-1]=='?' or text[len(text)-1]=='!' else potential_ans-1
 
 
@@ -28,4 +30,17 @@ def average_word_len(text:str):
 
 
 def top_k_n_grams(text:str,k=10, n=4):
-    pass
+    k, n = k, n if k > 0 and n > 0 else 10, 4
+    ngram_dict = {} # key: ngram, value: count
+    words = tuple(re.split(r"[.!? \|/;:-=+*#$%']",text))
+    if(len(words)<n):
+        print("Incorrect input")
+        return
+    for i in range(len(words) - n + 1):
+        ngram = words[i:i+n]
+        ngram_dict[ngram] = ngram_dict[ngram] + 1 if ngram in ngram_dict.keys() else 1
+
+    result = sorted(ngram_dict.items(), key=lambda pair: pair[1] reverse=True)
+    return result[:k]    
+
+
